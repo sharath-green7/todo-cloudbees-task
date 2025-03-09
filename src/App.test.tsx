d08e831e57e9d47bf5c6e9d78bf670dc076a883d
sharath <sharath.green76@gmail.com>
2025-03-10 00:46:53 +0530

    RTL cases done for add and render todo.

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ToDoApp from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock localStorage
+beforeEach(() => {
+  localStorage.clear();
+  jest.spyOn(Storage.prototype, "setItem");
+  jest.spyOn(Storage.prototype, "getItem");
+});
+
+test("adds a new todo and updates the list", async () => {
+  render(<ToDoApp />);
+  
+  userEvent.type(screen.getByRole("textbox", { name: "" }), 'testTyping');
+  const addButton = screen.getByRole("button", { name: "+ Add" });
+  await userEvent.click(addButton);
+  expect(screen.getByDisplayValue("testTyping")).toBeInTheDocument();
+});
+
+test("loads todos from localStorage on mount", async () => {
+  render(<ToDoApp />);
+  userEvent.type(screen.getByRole("textbox", { name: "" }), 'Saved Task');
+  const addButton = screen.getByRole("button", { name: "+ Add" });
+  await userEvent.click(addButton);
+  expect(screen.getByDisplayValue("Saved Task")).toBeInTheDocument();
 });
