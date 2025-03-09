d3a3910164fbec5da8a002502343d658036465f2
sharath <sharath.green76@gmail.com>
2025-03-09 16:56:43 +0530

    Todo list and item with styling done.

---
+++ src/components/ToDoItem.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { TodoItemInterface } from "../inteface";
+
+const ToDoItem = (props: TodoItemInterface) => {
+  return (
+    <div className="todo-item">
+      <div onClick={() => props.handleTodoComplete(props.todo.id)}>
+        {props.todo.isCompleted ? (
+          <span className="todo-item-checked">✔</span>
+        ) : (
+          <span className="todo-item-unchecked" />
+        )}
+      </div>
+      <div className="todo-item-input-wrapper">
+        <input
+          value={props.todo.name}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            props.handleTodoUpdate(event, props.todo.id)
+          }
+        />
+      </div>
+      <div
+        className="item-remove"
+        onClick={() => props.handleTodoRemove(props.todo.id)}
+      >
+        ⨯
+      </div>
+    </div>
+  );
+};
+export default ToDoItem;
